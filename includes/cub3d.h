f62c32f5d6adb5a45e7c30a3cdb6205485ad3773
pvineet44@gmail.com <vparekh@e1r8p7.42.fr>
2020-01-10 17:59:56 +0100

    dir structure

---
+++ includes/cub3d.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vparekh <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/10 17:53:11 by vparekh           #+#    #+#             */
+/*   Updated: 2020/01/10 17:55:33 by vparekh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+# include "mlx.h"
+# include "libft.h"
+# include "get_next_line.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+#endif
